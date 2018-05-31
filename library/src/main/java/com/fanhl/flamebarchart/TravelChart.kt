86d9eb5007c9b490e947f8134d6a2577c013ae47
fanhl <fanhonglin@klicen.com>
2018-05-31 17:35:48 +0800

    TravelChart

--- library/src/main/java/com/fanhl/flamebarchart/TravelChart.kt
+++ library/src/main/java/com/fanhl/flamebarchart/TravelChart.kt
@@ -1,4 +1,30 @@
 package com.fanhl.flamebarchart
 
-class TravelChart {
+import android.content.Context
+import android.graphics.Canvas
+import android.graphics.Color
+import android.graphics.Paint
+import android.graphics.Path
+import android.util.AttributeSet
+import android.view.View
+import android.widget.OverScroller
+
+/**
+ * 数据图表
+ *
+ * @author fanhl
+ */
+class TravelChart @JvmOverloads constructor(
+        context: Context,
+        attrs: AttributeSet? = null,
+        defStyleAttr: Int = 0
+) : View(context, attrs, defStyleAttr) {
+    private val paint by lazy { Paint() }
+    private val path by lazy { Path() }
+    private val scroller by lazy { OverScroller(context) }
+
+    override fun onDraw(canvas: Canvas) {
+        super.onDraw(canvas)
+        canvas.drawColor(Color.RED)
+    }
 }
