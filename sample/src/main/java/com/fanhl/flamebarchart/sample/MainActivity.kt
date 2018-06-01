b40f6ffba9f6c910716ee31f8fa4624d91fae874
fanhl <fanhonglin@klicen.com>
2018-06-01 15:38:56 +0800

    drawable

--- sample/src/main/java/com/fanhl/flamebarchart/sample/MainActivity.kt
+++ sample/src/main/java/com/fanhl/flamebarchart/sample/MainActivity.kt
@@ -2,11 +2,30 @@ package com.fanhl.flamebarchart.sample
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.fanhl.flamebarchart.TravelChart
+import kotlinx.android.synthetic.main.activity_main.*
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val random = Random()
+        chart_travel.data = TravelChart.DefaultData<Item>().apply {
+            (1..20).forEach { list.add(Item(random.nextFloat())) }
+        }
+    }
+
+    data class Item(val y: Float) : TravelChart.IItem {
+        override fun getXLabel(): String {
+            return "-$y-"
+        }
+
+        override fun getYAxis(): Float {
+            return y
+        }
+
     }
 }
